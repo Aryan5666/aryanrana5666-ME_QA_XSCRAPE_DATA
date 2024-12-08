8613858e145d4fd7dd0b76fcc6f0fd88c77b95bf
Aryan Rana <aryanrana5666@gmail.com>
2024-12-08 21:33:35 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/Utilities.java
+++ src/test/java/demo/Utilities.java
@@ -14,14 +14,14 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import demo.wrappers.SeleniumWrapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import demo.wrappers.SeleniumWrapper;
 
 public class Utilities {
 
     public static void scrape(String year, WebDriver driver) {
         try {
-            System.out.println("Starting data extraction for year: " + year);
 
             WebElement yearLink = driver.findElement(By.id(year));
             String yearLinkText = yearLink.getText();
@@ -32,53 +32,57 @@ public class Utilities {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@class='table']")));
 
             ArrayList<HashMap<String, String>> movieList = new ArrayList<>();
+
             List<WebElement> filmRows = driver.findElements(By.xpath("//tr[@class='film']"));
-
+            // Declare int count = 0;
+            int count = 0;
             for (WebElement filmRow : filmRows) {
-                try {
-                    String filmTitle = filmRow.findElement(By.xpath("./td[contains(@class,'title')]")).getText();
-                    String nomination = filmRow.findElement(By.xpath("./td[contains(@class,'nominations')]")).getText();
-                    String awards = filmRow.findElement(By.xpath("./td[contains(@class,'awards')]")).getText();
-                    boolean isWinner = filmRow.getAttribute("class").contains("best-picture");
-                    long epoch = System.currentTimeMillis() / 1000;
+                String filmTitle = filmRow.findElement(By.xpath("./td[contains(@class,'title')]")).getText();
+                String nomination = filmRow.findElement(By.xpath("./td[contains(@class,'nominations')]")).getText();
+                String awards = filmRow.findElement(By.xpath("./td[contains(@class,'awards')]")).getText();
+                boolean isWinnerFlag = count == 0;
+                String isWinner = String.valueOf(isWinnerFlag);
 
-                    HashMap<String, String> movieMap = new HashMap<>();
-                    movieMap.put("epochTime", String.valueOf(epoch));
-                    movieMap.put("year", yearLinkText);
-                    movieMap.put("title", filmTitle);
-                    movieMap.put("nomination", nomination);
-                    movieMap.put("awards", awards);
-                    movieMap.put("isWinner", String.valueOf(isWinner));
+                long epoch = System.currentTimeMillis() / 1000;
+                String epochTime = String.valueOf(epoch);
 
-                    movieList.add(movieMap);
+                HashMap<String, String> movieMap = new HashMap<>();
+                movieMap.put("epochTime", epochTime);
+                movieMap.put("year", yearLinkText);
+                movieMap.put("title", filmTitle);
+                movieMap.put("nomination", nomination);
+                movieMap.put("awards", awards);
+                movieMap.put("isWinner", isWinner);
 
-                } catch (Exception e) {
-                    System.err.println("Error extracting data for a film row: " + e.getMessage());
-                }
+                movieList.add(movieMap);
+
+                count++;
             }
 
-            if (movieList.isEmpty()) {
-                System.out.println("No movie data found for year: " + year);
-            } else {
-                System.out.println("Collected " + movieList.size() + " movies for year: " + year);
+            // Print the collected data
+            for (HashMap<String, String> movieData : movieList) {
+                System.out.println("Epoch time: " + movieData.get("epochTime") + ", Year: " + movieData.get("year")
+                        + ", Film title: " + movieData.get("title") + ", Nomination: " + movieData.get("nomination")
+                        + ", Awards: " + movieData.get("awards")
+                        + ", Best Picture: " + movieData.get("isWinner"));
             }
 
-            // Save data to JSON
-            File outputDir = new File("output");
-            if (!outputDir.exists()) {
-                outputDir.mkdirs();
-            }
-
-            File jsonFile = new File(outputDir, year + "-oscar-winner-data.json");
+            // Store the HashMap Data in a json File
             ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(jsonFile, movieList);
-
-            System.out.println("JSON data written to: " + jsonFile.getAbsolutePath());
-            Assert.assertTrue(jsonFile.length() > 0, "Generated JSON file is empty.");
+            try {
+                File jsonFile = new File("/Users/zaynadee/Documents/selenium-starter-2/src/test/output/" + year
+                        + "-oscar-winner-data.json");
+                mapper.writeValue(jsonFile, movieList);
+                System.out.println("JSON data written to: " + jsonFile.getAbsolutePath());
+                Assert.assertTrue(jsonFile.length() != 0);
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
 
         } catch (Exception e) {
-            System.err.println("Web scraping for movies failed for year " + year + ": " + e.getMessage());
+            System.out.println("Web Scrap for movies is failed..");
             e.printStackTrace();
         }
     }
+
 }
--- src/test/java/demo/wrappers/SeleniumWrapper.java
+++ src/test/java/demo/wrappers/SeleniumWrapper.java
@@ -1,18 +1,8 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
 
 public class SeleniumWrapper {
 
